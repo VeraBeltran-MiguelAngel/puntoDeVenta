d3a38395895deb0befe3c9bc244491cd9213ead6
Miguel Angel Vera Beltrán <miguel-angel.vera-beltran@outlook.com>
2023-11-02 20:02:53 -0600

    Se documenta proceso de punto de venta

--- src/app/modules/recepcion/components/home/home.component.ts
+++ src/app/modules/recepcion/components/home/home.component.ts
@@ -8,8 +8,7 @@ import { Producto } from '../models/producto';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  selectedProducts: Producto[] = [];
+  selectedProducts: Producto[] = []; //lista de productos seleccionados
   totalAPagar: number = 0;
 
   displayedColumns: string[] = [
@@ -20,83 +19,98 @@ export class HomeComponent implements OnInit {
     'descripcion',
     'precio',
     'cantidad',
-    'acciones'
+    'acciones',
   ];
-  productData: Producto[]=[
+  productData: Producto[] = [
     {
       id: 1,
       categoria: 'Bebidas',
-      nombre:'Agua ciel',
+      nombre: 'Agua ciel',
       tamaño: '500 ml',
       descripcion: 'agua simple',
       precio: 9.5,
-      cantidad:0
+      cantidad: 0,
     },
     {
       id: 2,
       categoria: 'Bebidas',
-      nombre:'Agua bonafont',
+      nombre: 'Agua bonafont',
       tamaño: '250 ml',
       descripcion: 'agua simple',
       precio: 5.8,
-      cantidad:0
+      cantidad: 0,
     },
     {
       id: 3,
       categoria: 'Bebidas',
-      nombre:'Gatorade',
+      nombre: 'Gatorade',
       tamaño: '500 ml',
       descripcion: 'bebida deportiva',
       precio: 20.5,
-      cantidad:0
+      cantidad: 0,
     },
     {
       id: 4,
       categoria: 'Suplementos',
-      nombre:'Proteina marca tal',
+      nombre: 'Proteina marca tal',
       tamaño: '500 gr',
       descripcion: '30 scopes',
       precio: 250.5,
-      cantidad:0
+      cantidad: 0,
     },
     {
       id: 5,
       categoria: 'Suplementos',
-      nombre:'Creatina marca tal',
+      nombre: 'Creatina marca tal',
       tamaño: '500 gr',
       descripcion: '30 scopes',
       precio: 200.8,
-      cantidad:0
-    }
-    
+      cantidad: 0,
+    },
   ];
   dataSource = new MatTableDataSource(this.productData);
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
+  /**
+   * metodo para filtrar la informacion que escribe el usaurio
+   */
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  /**
+   * Metodo para ir agregando productos a la lista de productos seleccionados
+   * @param producto 
+   */
   agregaraBalance(producto: Producto) {
-    const productoExistente = this.selectedProducts.find((p) => p.id === producto.id);
-  
+    /**
+     * el método busca si el producto que se está intentando agregar
+     * existe en la lista de productos seleccionados (
+     */
+    const productoExistente = this.selectedProducts.find(
+      (p) => p.id === producto.id
+    );
+
+    /**
+     * Si se encuentra un producto existente con el mismo id, significa que el producto ya ha sido agregado 
+     * al carrito. En este caso, el código aumenta la cantidad del producto existente en la lista selectedProducts
+     *  al agregar la cantidad del nuevo producto (producto.cantidad) a la cantidad existente del producto.
+     */
     if (productoExistente) {
       productoExistente.cantidad += producto.cantidad;
     } else {
       this.selectedProducts.push({ ...producto });
     }
-  
+
     // Recalcular el total a pagar
-    this.totalAPagar = this.selectedProducts.reduce((total, p) => total + p.precio * p.cantidad, 0);
-  
+    this.totalAPagar = this.selectedProducts.reduce(
+      (total, p) => total + p.precio * p.cantidad,
+      0
+    );
+
     // Reiniciar la cantidad del producto
     producto.cantidad = 0;
   }
-  
-
-
 }
