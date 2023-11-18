ef5c534e2ba99fde0f78e89f095233a96440ca9c
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-11-18 10:53:52 -0600

    se manejan mensajes de exito y error con envio de correo
    Se atrapan correctamente los mensajes del api, se debe desactivar el debug del server de correo
    o no se podra visualizar correctamente los mensajes de exito o error

--- src/app/service/resetPassword.service.ts
+++ src/app/service/resetPassword.service.ts
@@ -6,14 +6,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ResetPasswordService {
-  API: string =
-    'https://olympus.arvispace.com/puntoDeVenta/conf/nuevaContra.php/';
+
+API :string ='http://localhost/EnviarMail/nuevaContra.php/';
+  // API: string ='https://olympus.arvispace.com/puntoDeVenta/conf/nuevaContra.php/';
   //para guardar los headers que manda el API
   httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private clienteHttp: HttpClient) {}
 
-  login(email: string): Observable<any> {
+  /**
+   * Metodo para enviar mail con link de reset password
+   * @param email 
+   * @returns 
+   */
+  enviarMail(email: string): Observable<any> {
     return this.clienteHttp.post(this.API + '?solicitaNuevaPass', email, {
       headers: this.httpHeaders,
     });
