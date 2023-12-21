51392a970931faf79df5c05075b41c038c08c287
17Marco00 <17antoniobol@gmail.com>
2023-12-21 09:59:57 -0600

    huellaInsercion
    Insercion y conectividad de huella

--- src/app/service/huella.service.ts
+++ src/app/service/huella.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
+import { InstruccionHuella } from './InstruccionHuella';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HuellaService {
 
-  URLServices: string = "https://olympus.arvispace.com/puntoDeVenta/conf/huella.php";   //http://localhost/plan/huella.php     //https://olympus.arvispace.com/puntoDeVenta/conf/huella.php    
+  URLServices: string = "http://192.168.100.37";  //http://192.168.100.37  //http://localhost/plan/huella.php     //https://olympus.arvispace.com/puntoDeVenta/conf/huella.php    
   constructor(private clienteHttp:HttpClient) { }
-
 
   registroHuella(id:any):Observable<any>{
     return this.clienteHttp.get(this.URLServices+"?NewHuella="+id);
   }  
+
+  insertarInstruccion(dato: InstruccionHuella):Observable<any>{
+    return this.clienteHttp.post(this.URLServices,dato);
+                                                /*+"?insertar=1" <- Nota: Colocarlo al cambiarlo a un servicio de php*/
+  }
 }
