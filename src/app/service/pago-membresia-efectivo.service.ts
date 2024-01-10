2bda950bbf1b4160322a519b63c7c4e82c9ff3e3
17Marco00 <17antoniobol@gmail.com>
2024-01-10 14:00:36 -0600

    update servicio
    Se actualizo el servicio de pago membresia efectivo

--- src/app/service/pago-membresia-efectivo.service.ts
+++ src/app/service/pago-membresia-efectivo.service.ts
@@ -8,7 +8,7 @@ import { HttpParams } from '@angular/common/http';
 })
 export class PagoMembresiaEfectivoService {
 
-  URLServices: string = "http://localhost/plan/pagoEfectivoMembresiaV2.php/";   //http://localhost/plan/pagoEfectivoMembresia.php/     //https://olympus.arvispace.com/puntoDeVenta/conf/pagoEfectivoMembresia.php/     
+  URLServices: string = "https://olympus.arvispace.com/puntoDeVenta/conf/pagoEfectivoMembresia.php/";   //http://localhost/plan/pagoEfectivoMembresia.php/     //https://olympus.arvispace.com/puntoDeVenta/conf/pagoEfectivoMembresia.php/     
   constructor(private clienteHttp:HttpClient) { }
 
   obternerDataMem(){
